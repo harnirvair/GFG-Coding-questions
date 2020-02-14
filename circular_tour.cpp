43518990a7c117158a8e12d39595d6fbe1a094c4
Harnirvair Singh <harnirvair@gmail.com>
2020-02-14 23:28:55 +0530

    added circular tour.cpp

---
+++ circular_tour.cpp
@@ -0,0 +1,15 @@
+int tour(petrolPump p[],int n)
+{
+   //Your code here
+   int start=0, deficit=0, capacity=0;
+   for(int i=0;i<n;i++){
+       capacity+=p[i].petrol-p[i].distance;
+       if(capacity<0){
+           start=i+1;
+           deficit+=capacity;
+           capacity=0;
+       }
+   }
+   return (capacity+deficit>0)?start:-1;
+   
+}
