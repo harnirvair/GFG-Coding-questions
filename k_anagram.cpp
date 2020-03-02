85525fcfab9b03eb3df1671718b29edaec06e8bf
Harnirvair Singh <harnirvair@gmail.com>
2020-03-02 22:02:24 +0530

    added new file

---
+++ k_anagram.cpp
@@ -0,0 +1,18 @@
+bool areKAnagrams(string str1, string str2, int k)
+  {
+    int h[26]={0};int c=0;
+    if(str1.length()!=str2.length())
+    return false;
+    for(int i=0;i<str1.length();i++){
+        h[str1[i]-'a']++;
+    }
+    for(int i=0;i<str2.length();i++){
+        if(h[str2[i]-'a']==0)
+        c++;
+        else
+        h[str2[i]-'a']--;
+    }
+    if(c<=k)
+    return true;
+    return false;
+   }
